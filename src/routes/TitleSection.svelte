80094040e2297f42ecc6becfd4656122a127d955
Squiggle7744 <107369399+Squiggle7744@users.noreply.github.com>
2022-08-21 13:25:54 -0700

    Removed arrows on search bar

--- src/routes/TitleSection.svelte
+++ src/routes/TitleSection.svelte
@@ -47,3 +47,17 @@
 		</span>
 	</h1>
 {/if}
+
+<style>
+	/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+</style>
