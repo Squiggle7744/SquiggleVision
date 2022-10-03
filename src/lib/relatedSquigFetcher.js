da32fde18b4e4f80e3835eeef903c700a98dcccd
Squiggle7744 <107369399+Squiggle7744@users.noreply.github.com>
2022-10-03 11:58:34 -0700

    troubleshooting df

--- src/lib/relatedSquigFetcher.js
+++ src/lib/relatedSquigFetcher.js
@@ -21,24 +21,28 @@ export const getSquigByAttribute = async (atty, attyValue) => {
 			atty = 'color_spread';
 			break;
 	}
+	console.log('importing df...');
+
 	let df = await DataFrame.fromCSV(
 		'https://raw.githubusercontent.com/Squiggle7744/Squigs.xyz/main/src/lib/allSquigs.csv'
 	);
+	console.log('df successfully imported!', df.show(3));
 
+	
 	const selectedDF = df.select('token_id', atty);
 
 	const selectedValuesDF = selectedDF.chain((row) => row.get(atty) === attyValue);
-	// selectedValuesDF.show(10);
+	selectedValuesDF.show(10);
 	const filteredToken_id_df = selectedValuesDF.toArray('token_id');
 
 	let totalLength = filteredToken_id_df.length;
-	// console.log('total length is... ', totalLength);
+	console.log('total length is... ', totalLength);
 	//Generate RNG between 0 and length of DF
 	let randomIndex = Math.floor(Math.random() * totalLength);
-	// console.log("random index we're choosing is... ", randomIndex);
+	console.log("random index we're choosing is... ", randomIndex);
 
 	tokenID = filteredToken_id_df[randomIndex];
-	// console.log('tokenID is... ', tokenID);
+	console.log('tokenID is... ', tokenID);
 
 	return tokenID;
 };
